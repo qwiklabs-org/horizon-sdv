6332791d759108389f839903bbcb5650a5c30b32
Kania <marta.kania@accenture.com>
2024-09-13 13:22:55 +0200

    update provider info

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,8 +1,8 @@
 provider "google" {
   project = "sdvc-2108202401"
-  region  = "europe-west1"
+  region  = "europe-west1-d"
 }
 provider "google-beta" {
   project = "sdvc-2108202401"
-  region  = "europe-west1"
+  region  = "europe-west1-d"
 }
