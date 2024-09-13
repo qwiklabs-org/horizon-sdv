67e1dda5a5941b6a2b261bb72725f1dbe9b4b071
Kania <marta.kania@accenture.com>
2024-09-13 14:40:52 +0200

    revert provider info change

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,8 +1,8 @@
 provider "google" {
   project = "sdvc-2108202401"
-  region  = "europe-west1-d"
+  region  = "europe-west1"
 }
 provider "google-beta" {
   project = "sdvc-2108202401"
-  region  = "europe-west1-d"
+  region  = "europe-west1"
 }
