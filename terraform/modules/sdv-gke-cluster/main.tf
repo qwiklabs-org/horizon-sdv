317ebd3637b0cf1b209559c0ee23734c35e9807b
Edson Schlei <edson.schlei@accenture.com>
2024-09-25 17:21:51 +0200

    Remove public IP from cluster

--- terraform/modules/sdv-gke-cluster/main.tf
+++ terraform/modules/sdv-gke-cluster/main.tf
@@ -11,8 +11,9 @@ resource "google_container_cluster" "default" {
   deletion_protection = false
 
   private_cluster_config {
-    enable_private_nodes   = true
-    master_ipv4_cidr_block = "10.0.0.0/28"
+    enable_private_nodes    = true
+    enable_private_endpoint = true
+    master_ipv4_cidr_block  = "10.0.0.0/28"
   }
 
   addons_config {
