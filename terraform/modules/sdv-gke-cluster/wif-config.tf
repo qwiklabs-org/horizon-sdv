a9695ed8db32d826c8b62795930c2723d828b9b7
Edson Schlei <edson.schlei@accenture.com>
2024-10-10 11:13:58 +0200

    taa-183: Create wif configuration

--- terraform/modules/sdv-gke-cluster/wif-config.tf
+++ terraform/modules/sdv-gke-cluster/wif-config.tf
@@ -1,23 +1,59 @@
 
+resource "google_iam_workload_identity_pool" "pool" {
+  workload_identity_pool_id = "sdv-wif-pool"
+  display_name              = "SDV Identity Pool"
+  description               = "A WIF pool for SDV cluster"
+}
 
-# resource "google_iam_workload_identity_pool" "pool" {
-#   workload_identity_pool_id = "my-pool"
-#   display_name              = "My Workload Identity Pool"
-#   description               = "A pool for my GKE cluster"
-# }
+resource "google_iam_workload_identity_pool_provider" "provider" {
+  workload_identity_pool_id          = google_iam_workload_identity_pool.pool.workload_identity_pool_id
+  workload_identity_pool_provider_id = "wip-provider"
+  display_name                       = "OIDC Provider"
+  description                        = "A WIP provider for SDV cluster"
 
-# resource "google_iam_workload_identity_pool_provider" "provider" {
-#   workload_identity_pool_id = google_iam_workload_identity_pool.pool.workload_identity_pool_id
-#   workload_identity_pool_provider_id = "my-provider"
-#   display_name               = "My OIDC Provider"
-#   description                = "A provider for my GKE cluster"
+  oidc {
+    issuer_uri = "https://container.googleapis.com/v1/projects/${data.google_project.project.project_id}/locations/${var.location}/clusters/${var.cluster_name}"
+  }
 
-#   oidc {
-#     issuer_uri = "https://container.googleapis.com/v1/projects/${var.project_id}/locations/${var.region}/clusters/${var.cluster_name}"
-#   }
+  attribute_mapping = {
+    "google.subject" = "assertion.sub"
+    "attribute.aud"  = "assertion.aud"
+  }
+}
 
-#   attribute_mapping = {
-#     "google.subject" = "assertion.sub"
-#     "attribute.aud"  = "assertion.aud"
+resource "google_service_account" "sa" {
+  project      = data.google_project.project.project_id
+  account_id   = "sdv-wif-sa"
+  display_name = "SDV SA for WIF"
+}
+
+resource "google_service_account_iam_member" "binding" {
+  service_account_id = google_service_account.sa.name
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.pool.name}/attribute.aud/${var.cluster_name}"
+}
+
+resource "google_project_iam_member" "artifact_registry" {
+  project = data.google_project.project.project_id
+  role    = "roles/artifactregistry.reader"
+  member  = "serviceAccount:${google_service_account.sa.email}"
+}
+
+resource "google_project_iam_member" "storage" {
+  project = data.google_project.project.project_id
+  role    = "roles/storage.objectUser"
+  member  = "serviceAccount:${google_service_account.sa.email}"
+}
+
+#
+# TODO: this on the kubernetes
+#
+# resource "kubernetes_service_account" "ksa" {
+#   metadata {
+#     name      = "sdv-wif-kubernetes-sa"
+#     namespace = "default"
+#     annotations = {
+#       "iam.gke.io/gcp-service-account" = google_service_account.sa.email
+#     }
 #   }
 # }
