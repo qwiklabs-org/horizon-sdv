e1ecbdd4fc19be2f4844d6d2bb99b245fa1d021b
Edson Schlei <edson.schlei@accenture.com>
2024-10-29 14:21:34 +0100

    taa-229: add project id in the ssh command line

--- terraform/modules/sdv-bash-on-bastion-host/main.tf
+++ terraform/modules/sdv-bash-on-bastion-host/main.tf
@@ -7,7 +7,7 @@ resource "null_resource" "execute_commands_on_bastion" {
 
   provisioner "local-exec" {
     command = <<EOT
-      gcloud compute ssh sdv-bastion-host --zone=europe-west1-d --command="
+      gcloud compute ssh sdv-bastion-host --zone=europe-west1-d --project=sdva-2108202401 --command="
         echo 'Executing commands on the bastion host...'
         touch ~/terraform-log.log
         echo $(date) >> ~/terraform-log.log
