f87462a8a19db3f48e80a547d0a2fba2c8da910c
Edson Schlei <edson.schlei@accenture.com>
2024-11-05 16:02:34 +0100

    taa-229: Using gcloud beta

--- terraform/modules/sdv-bash-on-bastion-host/main.tf
+++ terraform/modules/sdv-bash-on-bastion-host/main.tf
@@ -8,7 +8,7 @@ resource "null_resource" "execute_bash_commands" {
     command = <<EOT
       echo "Executing bash commands..."
 
-      gcloud compute ssh sdv-bastion-host --zone=europe-west1-d --project=sdva-2108202401 --command="
+      gcloud beta compute ssh sdv-bastion-host --zone=europe-west1-d --project=sdva-2108202401 --command="
       echo 'Executing commands on the bastion host...'
       touch ~/terraform-log.log
       echo $(date) >> ~/terraform-log.log
