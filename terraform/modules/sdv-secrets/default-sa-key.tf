6adb12800364f2ef16846e65450d7855ea29017a
Edson Schlei <edson.schlei@accenture.com>
2024-11-12 14:38:07 +0100

    taa-269: Add sa-key access to jenkins

--- terraform/modules/sdv-secrets/default-sa-key.tf
+++ terraform/modules/sdv-secrets/default-sa-key.tf
@@ -1,4 +1,4 @@
-data "google_project" "project" {}
+# data "google_project" "project" {}
 
 resource "google_service_account_key" "sa_key" {
   service_account_id = "268541173342-compute@developer.gserviceaccount.com"
@@ -20,4 +20,12 @@ resource "google_secret_manager_secret" "sa_key_secret" {
 resource "google_secret_manager_secret_version" "sa_key_secret_version" {
   secret      = google_secret_manager_secret.sa_key_secret.id
   secret_data = base64decode(google_service_account_key.sa_key.private_key)
+}
+
+resource "google_secret_manager_secret_iam_member" "member" {
+  project = google_secret_manager_secret.sa_key_secret.project
+  secret_id = google_secret_manager_secret.sa_key_secret.secret_id
+  role = "roles/secretmanager.secretAccessor"
+  ## ns/jenkins/jenkins-sa.
+  member = "principal://iam.googleapis.com/projects/${data.google_project.project.number}/locations/global/workloadIdentityPools/${data.google_project.project.project_id}.svc.id.goog/subject/ns/jenkins/sa/jenkins-sa"
 }
