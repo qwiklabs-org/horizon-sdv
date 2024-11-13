f815359686f613392adcefc574cf7c4a89018fe2
Edson Schlei <edson.schlei@accenture.com>
2024-11-13 14:30:31 +0100

    taa-265: Add dev to the names

--- terraform/modules/sdv-certificate-manager/main.tf
+++ terraform/modules/sdv-certificate-manager/main.tf
@@ -1,12 +1,12 @@
 
 resource "google_certificate_manager_certificate_map" "horizon_sdv_map" {
-  name        = "horizon-sdv-map"
+  name        = "horizon-sdv-map-dev"
   description = "Certificate Manager Map for Horizon SDV"
 }
 
 resource "google_certificate_manager_certificate_map_entry" "horizon_sdv_entry" {
   map         = google_certificate_manager_certificate_map.horizon_sdv_map.name
-  name        = "horizon-sdv-entry"
+  name        = "horizon-sdv-dev-entry"
   hostname    = "dev.horizon-sdv.scpmtk.com"
   certificates = ["horizon-sdv"]
 }
