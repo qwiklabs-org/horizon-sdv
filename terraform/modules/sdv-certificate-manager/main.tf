2e65cca48c1b1071e43c6a3e16f953928a0f2c6a
Edson Schlei <edson.schlei@accenture.com>
2024-11-14 10:52:58 +0100

    taa-265: Another try to configure certificate

--- terraform/modules/sdv-certificate-manager/main.tf
+++ terraform/modules/sdv-certificate-manager/main.tf
@@ -1,3 +1,11 @@
+resource "google_certificate_manager_certificate" "horizon_sdv_cert" {
+  name        = "horizon-sdv"
+  # description = "Certificate for Horizon SDV"
+  scope       = "DEFAULT"
+  managed {
+    domains = ["dev.horizon-sdv.scpmtk.com"]
+  }
+}
 
 resource "google_certificate_manager_certificate_map" "horizon_sdv_map" {
   name        = "horizon-sdv-map-dev"
@@ -8,6 +16,5 @@ resource "google_certificate_manager_certificate_map_entry" "horizon_sdv_entry" 
   map         = google_certificate_manager_certificate_map.horizon_sdv_map.name
   name        = "horizon-sdv-dev-entry"
   hostname    = "dev.horizon-sdv.scpmtk.com"
-  certificates = ["horizon-sdv"]
+  certificates = [google_certificate_manager_certificate.horizon_sdv_cert.name]
 }
-
