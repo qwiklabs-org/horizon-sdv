8768f138a70e4a5c572a7351bcee5321fef1a4fc
Edson Schlei <edson.schlei@accenture.com>
2024-11-15 13:06:44 +0100

    taa-270: Copy file to storage and then to the bastion host from storage

--- terraform/modules/sdv-copy-to-bastion-host/main.tf
+++ terraform/modules/sdv-copy-to-bastion-host/main.tf
@@ -10,6 +10,27 @@ resource "terraform_data" "debug_file_content" {
   input = local.file_content
 }
 
+resource "google_storage_bucket" "bucket" {
+  name                        = "${data.google_project.project.project_id}-scripts"
+  location                    = var.location
+  uniform_bucket_level_access = true
+}
+
+resource "google_project_iam_member" "storage_object_viewer" {
+  project = data.google_project.project.project_id
+  role    = "roles/storage.objectViewer"
+  member  = "serviceAccount:sdv-bastion-host-sa-iap@sdva-2108202401.iam.gserviceaccount.com"
+}
+
+
+# Example usage in a Google Cloud resource (e.g., a Storage Bucket object)
+resource "google_storage_bucket_object" "horizon_stage_01_sh" {
+  name       = "bash-scripts/horizon-stage-01.sh"
+  bucket     = google_storage_bucket.bucket.name
+  content    = local.file_content
+  depends_on = [google_storage_bucket.bucket]
+}
+
 resource "null_resource" "copy_to_bastion" {
   triggers = {
     always_run = "${timestamp()}"
@@ -18,22 +39,10 @@ resource "null_resource" "copy_to_bastion" {
   provisioner "local-exec" {
     command = <<EOT
       gcloud beta compute ssh ${var.bastion_host} --zone=${var.zone} --project=${data.google_project.project.project_id} --command="
-      echo ${local.file_content} > ${var.destination_path}
+      gsutil cp gs://sdva-2108202401-scripts/bash-scripts/horizon-stage-01.sh .
     "
     EOT
   }
-}
 
-resource "google_storage_bucket" "bucket" {
-  name                        = "${data.google_project.project.project_id}-scripts"
-  location                    = var.location
-  uniform_bucket_level_access = true
-}
-
-
-# Example usage in a Google Cloud resource (e.g., a Storage Bucket object)
-resource "google_storage_bucket_object" "horizon_stage_01_sh" {
-  name   = "bash-scripts/horizon-stage-01.sh"
-  bucket = google_storage_bucket.bucket.name
-  content = local.file_content
+  depends_on = [google_storage_bucket_object.horizon_stage_01_sh]
 }
