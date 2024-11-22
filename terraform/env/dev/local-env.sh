daf9ab88c6dfce510573172680835434c6b4cf46
Edson Schlei <edson.schlei@accenture.com>
2024-11-22 11:19:29 +0100

    taa-301: add gh access token

--- terraform/env/dev/local-env.sh
+++ terraform/env/dev/local-env.sh
@@ -9,3 +9,6 @@ export TF_VAR_sdv_gh_argocd_initial_password="123"
 export TF_VAR_sdv_gh_jenkins_initial_password="123"
 export TF_VAR_sdv_gh_keycloak_initial_password="123"
 export TF_VAR_sdv_gh_mtkc_regcred="123"
+
+# Github access token
+export TF_VAR_sdv_gh_access_token="123"
