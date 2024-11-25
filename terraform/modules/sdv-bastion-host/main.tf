8b7b438b3ee2e0819f2e80e9433cc82ceb2c140b
lukaszdom <lukasz.domke@accenture.com>
2024-11-25 16:13:28 +0100

    adding 2 additional permission to bastion host sa

--- terraform/modules/sdv-bastion-host/main.tf
+++ terraform/modules/sdv-bastion-host/main.tf
@@ -89,3 +89,15 @@ resource "google_project_iam_member" "artifact_registry_reader" {
   role    = "roles/artifactregistry.reader"
   member  = "serviceAccount:${google_service_account.vm_sa.email}"
 }
+
+resource "google_project_iam_member" "artifact_registry_writer" {
+  project = data.google_project.project.project_id
+  role    = "roles/artifactregistry.writer"
+  member  = "serviceAccount:${google_service_account.vm_sa.email}"
+}
+
+resource "google_project_iam_member" "artifact_secret_accessor" {
+  project = data.google_project.project.project_id
+  role    = "roles/secretmanager.secretAccessor"
+  member  = "serviceAccount:${google_service_account.vm_sa.email}"
+}
