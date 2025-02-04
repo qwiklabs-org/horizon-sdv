0ecaa5e53d7a05c38a7ff4a35b4f4247922f9f0f
Wojciech Kobryn <wojciech.kobryn@accenture.com>
2025-02-04 12:45:55 +0100

    Adding docker build, gateway for argocd and jenkins pvc creates earlier

---
+++ gitops/env/stage2/configs/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CLOUD_REGION=europe-west1
+PROJECT_ID=sdvc-2108202401
+VERSION=1.0.0
+
+declare -a configs=("gerrit-post" "mtk-connect-post" "keycloak-post" "keycloak-post-gerrit" "keycloak-post-jenkins" "keycloak-post-mtk-connect")
+substr="-post"
+for config in "${configs[@]}"; do
+  docker build -t ${CLOUD_REGION}-docker.pkg.dev/${PROJECT_ID}/horizon-sdv/${config}:${VERSION} ${config%$substr*}/${config}
+  docker push ${CLOUD_REGION}-docker.pkg.dev/${PROJECT_ID}/horizon-sdv/${config}:${VERSION}
+done
