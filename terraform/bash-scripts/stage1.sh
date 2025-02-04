9300860d18d6a160a17b2ff1c3102b340ce2eb77
Wojciech Kobryn <wojciech.kobryn@accenture.com>
2025-02-04 01:50:49 +0100

    stage1 repo url change

--- terraform/bash-scripts/stage1.sh
+++ terraform/bash-scripts/stage1.sh
@@ -2,7 +2,7 @@
 
 CLOUD_REGION=europe-west1
 CLUSTER_NAME="sdv-cluster"
-REPOSITORY=github.com/AGBG-ASG/acn-horizon-sdv-android-platform-manifest
+REPOSITORY=github.com/AGBG-ASG/acn-horizon-sdv
 
 touch ~/terraform-log.log
 echo $(date) >>~/terraform-log.log
@@ -68,4 +68,4 @@ git clone https://x-access-token:${GITHUB_ACCESS_TOKEN}@${REPOSITORY} ~/horizon-
 echo ""
 echo "List current branch and remote"
 cd ~/horizon-sdv
-git branch -vva
+git checkout -t origin/env/sbx
