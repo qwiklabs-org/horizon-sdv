b0281001478144aac3370e9d6ac8912e886f5286
lukaszdom <lukasz.domke@accenture.com>
2025-02-08 10:13:46 +0100

    TAA-499 draft7

--- terraform/env/variables.tf
+++ terraform/env/variables.tf
@@ -4,11 +4,6 @@ variable "sdv_gh_app_id" {
   type        = string
 }
 
-variable "sdv_gh_installation_id" {
-  description = "The var gh_installation_id value"
-  type        = string
-}
-
 variable "sdv_gh_app_key" {
   description = "The secret GH_APP_KEY value"
   type        = string
