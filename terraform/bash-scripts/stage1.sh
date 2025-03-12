5ad43092211a41f16ec73b4a80961a10987c99a3
p-habib-eshwar <p.habib.eshwar@accenture.com>
2025-03-12 10:39:05 +0530

    TAA-112: Add Apache 2.0 license header for files to be released where applicable (updated with standard format) (#368)
    * update licenses with removed whitespaces, corrected placement for shell scripts and removed misplaced description.
    
    * use env to locate bash in shebang.

--- terraform/bash-scripts/stage1.sh
+++ terraform/bash-scripts/stage1.sh
@@ -1,11 +1,13 @@
-# Copyright (c) 2024-2025 Accenture, All Rights Reserved.
+#!/usr/bin/env bash
 
+# Copyright (c) 2024-2025 Accenture, All Rights Reserved.
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-
+#
 #         http://www.apache.org/licenses/LICENSE-2.0
-
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,8 +24,6 @@
 # 2. Establish connection to the GKE cluster.
 # 3. Setup docker with required permission and perform a docker pull check.
 # 4. Clone the GitHub repository for further setup.
-
-#!/bin/bash
 
 CLUSTER_NAME="sdv-cluster"
 REPOSITORY=github.com/${GITHUB_REPO_NAME}
